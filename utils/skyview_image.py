a08d8b33f629b0da20cb8ff827ef394a4ed06c89
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2022-10-21 18:09:31 +0530

    Create skyview_image.py

---
+++ utils/skyview_image.py
@@ -0,0 +1,44 @@
+import argparse
+import astropy
+import matplotlib.pyplot as plt
+from astroquery.skyview import SkyView
+from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name_loc', type=str)
+    parser.add_argument('--surveyname', type=str, default=None)
+    parser.add_argument('--surveyname', type=str, default=None)
+
+    args = parser.parse_args()
+    
+    name_loc=args.name_loc
+    surveyname=args.surveyname
+    
+    if surveyname is None:
+        # Query for images centered on target name
+        hdu = SkyView.get_images(name_loc, survey='DSS')[0][0]
+    else:
+        print(SkyView.survey_dict)
+        print('Enter name of survey:')
+        survey_name=str(input())
+        # Query for images centered on target name
+        hdu = SkyView.get_images(name_loc, survey=survey_name)[0][0]
+        
+    # Tell matplotlib how to plot WCS axes
+    wcs = WCS(hdu.header)
+    fig = plt.figure(figsize=(20, 10), dpi=100)
+    ax = fig.gca(projection=wcs)
+    # Plot the image
+    ax.imshow(hdu.data)
+    ax.set(xlabel="RA", ylabel="Dec")
+    fig.savefig('skyimage.jpeg')
+        
+        
+    
+    
+  
+    
+    
